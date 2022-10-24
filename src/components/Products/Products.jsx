59135c42272b9866f9dc2adb9e33328e2cd24a7c
Albrin Richard <albrinrichard@Albrins-MacBook-Air.local>
2022-10-23 22:58:13 -0700

    all working page

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Grid  from "@material-ui/core";
+
+import React from 'react'
+
+const Products = () => {
+    <main>
+        <grid>
+
+        </grid> 
+    </main>
+  
+}
+
+export default Products
